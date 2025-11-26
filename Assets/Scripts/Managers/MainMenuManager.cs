9651ddb2679a7ed7d2427d46f31b0a735b411ed5
genihe <genihe@hotmail.com>
2025-11-25 22:37:43 -0300

    Se añadio al MainMenu un boton para visualizar los creditos

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/* using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] Button buttonNewGame;
+    [SerializeField] Button creditsButton;
     [SerializeField] Button buttonQuitGame;
     AsyncOperation asyncLoad;
 
     void Start()
     {
         buttonNewGame.onClick.AddListener(ActivateScene);
+        creditsButton.onClick.AddListener(ActivateScene);
         buttonQuitGame.onClick.AddListener(QuitGame);
         StartCoroutine(PreloadScene("Level_1"));
     }
@@ -35,6 +37,65 @@ public class MainMenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(levelName);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+} */
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Botones de Menú")]
+    [SerializeField] Button buttonNewGame;
+    [SerializeField] Button creditsButton;
+    [SerializeField] Button buttonQuitGame;
+
+    private AsyncOperation asyncLoad;
+
+    void Start()
+    {
+        // Asignar eventos a los botones
+        buttonNewGame.onClick.AddListener(ActivateScene);
+        creditsButton.onClick.AddListener(() => LoadLevel("Credits"));
+        buttonQuitGame.onClick.AddListener(QuitGame);
+
+        // Comenzar precarga de la escena del juego
+        StartCoroutine(PreloadScene("Level_1"));
+    }
+
+    IEnumerator PreloadScene(string sceneName)
+    {
+        asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+        
+        // Esperar hasta que la escena esté lista para activarse
+        while (asyncLoad.progress < 0.9f)
+        {
+            yield return null;
+        }
+    }
+
+    // Activar la escena pre-cargada (Level_1)
+    void ActivateScene()
+    {
+        if (asyncLoad != null)
+        {
+            asyncLoad.allowSceneActivation = true;
+        }
+    }
+
+    // Cargar escena directamente (para créditos, sin precarga)
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    // Salir del juego
     public void QuitGame()
     {
         Application.Quit();
